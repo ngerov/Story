b13a2c69a0f3ec58da021288f75b8a626dc37299
ngerov <nikolagerov75@gmail.com>
2025-06-09 10:09:57 +0300

    Chapter 2 and 3 completed.

---
+++ main.py
@@ -0,0 +1,7 @@
+### Intro Version control and Git
+
+# create an empty git repository -> git init
+# files in the git staging area -> git status, untracked files in red
+# adding files in the staging area -> git add filename
+# commiting files to a new save point -> git commit -m "Commit message"
+# seeing last commits -> git log
